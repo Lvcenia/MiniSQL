5871cdaba5e7f73a2d3aae1afcf5ee58d7cfa35f
陈文宇 <cwy1119@qq.com>
2020-05-31 22:26:35 +0800

    统一文件操作异常类为FileOperateException

--- MiniSQL/BufferManager.h
+++ MiniSQL/BufferManager.h
@@ -8,27 +8,17 @@
 using std::string;
 using std::exception;
 
-//文件打开时异常
-class FileOpenException :public exception
+//文件操作异常
+class FileOperateException :public exception
 {
 private:
 	string errorLog;
 public:
-	FileOpenException() :errorLog("Fail to open file") {}
-	FileOpenException(string filename) :errorLog("Fail to open " + filename) {}
+	FileOperateException(string description) :errorLog(description) {}
+	FileOperateException(string description,string filename) :errorLog(description + filename) {}
 	virtual const char* what()const { return errorLog.c_str(); }
 };
 
-//文件移除时异常
-class FileRemoveException :public exception
-{
-private:
-	string errorLog;
-public:
-	FileRemoveException() :errorLog("Fail to remove file") {}
-	FileRemoveException(string filename) :errorLog("Fail to remove " + filename) {}
-	virtual const char* what()const { return errorLog.c_str(); }
-};
 
 class BufferManager
 {
