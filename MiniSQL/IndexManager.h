6e36d79c6a348ce196af53dee1d5ea7d684e2d36
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2020-06-01 20:26:04 +0800

    定义了IndexManager接口

--- MiniSQL/IndexManager.h
+++ MiniSQL/IndexManager.h
@@ -1,5 +1,43 @@
 #pragma once
-class IndexManager
-{
+#include <map>
+#include <string>
+#include <list>
+#include "BPTree/BPTree.h"
+#include "Generals.h"
+#include "BufferManager.h"
+using namespace std;
+
+#define BLOCKSIZE 4096
+#define INT_STRING_SIZE 10
+#define FLOAT_INTEGER_SIZE 10 //Integer part length of a float number
+#define FLOAT_DECIMAL_SIZE 9//decimal part length of a float number
+#define HEADER_BLOCK_OFFSET 1*BLOCKSIZE//record offset
+
+typedef unsigned char BYTE;
+
+struct INDEXFILEHEADER{
+	BYTE indexName[32];
+	Type type;
+	int attributeLength;
+	int offsetInRecord;
+	int fanOut;
+	int elementCount;
 };
 
+class IndexManager {
+public:
+	IndexManager();
+	IndexManager(list<string> indexNames);
+	virtual ~IndexManager();
+	void createIndex(string indexName, string attributeName, string tableName);
+	void dropIndex(string name);
+
+private:
+	map<string, BPTree*> indexDictionary;
+	BufferManager* bufferManager;
+
+	void CreateFromFile(string name);
+	void SaveToFile(string name);
+	int GetEndOffset(string fileName);
+	int GetNextToEndOffset(string fileName, int recordLength);
+};
