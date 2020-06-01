7153c2b19f1878fb7983098d7050c5f90048d856
陈文宇 <cwy1119@qq.com>
2020-06-01 18:24:13 +0800

    实现了BufferManager接口的定义

--- MiniSQL/ArrayList.h
+++ MiniSQL/ArrayList.h
@@ -1,38 +1,21 @@
-#include "ArrayList.h"
+#ifndef _ARRAYLIST_
+#define _ARRAYLIST_
 
-ArrayList::ArrayList() :
-	head(0), tail(BLOCKNUM - 1)
+#define BLOCKSIZE 4096
+#define BLOCKNUM 10
+
+class ArrayList
 {
-	for (int i = 0; i < BLOCKNUM; i++) {
-		arraylist[i].last = i - 1;
-		arraylist[i].next = i + 1;
-	}
-	arraylist[tail].next = -1;
-}
-
-
-ArrayList::~ArrayList()
-{
-}
-
-int ArrayList::getHead()
-{
-	return head;
-}
-
-void ArrayList::moveToTail(int index)
-{
-	if (index == tail)
-		return;
-	int last = arraylist[index].last;
-	int next = arraylist[index].next;
-
-	arraylist[tail].next = index;
-	arraylist[last].next = next;
-	arraylist[next].last = last;
-	arraylist[index].next = -1;
-	arraylist[index].last = tail;
-	tail = index;
-	if (index == head)
-		head = next;
-}
+private:
+	int head, tail;
+	struct {
+		int last;
+		int next;
+	}arraylist[BLOCKNUM];
+public:
+	ArrayList();
+	~ArrayList();
+	void moveToTail(int index);
+	int getHead();
+};
+#endif // !_ARRAYLIST_
--- MiniSQL/BufferManager.h
+++ MiniSQL/BufferManager.h
@@ -4,7 +4,10 @@
 #include <string>
 #include <cstdio>
 #include <exception>
+#include "ArrayList.h"
 
+typedef unsigned char BYTE;
+typedef int ADDRESS;
 using std::string;
 using std::exception;
 
@@ -19,9 +22,62 @@ public:
 	virtual const char* what()const { return errorLog.c_str(); }
 };
 
+//数据块全部占用异常
+class BlockAllPinnedException : public std::exception
+{
+private:
+	std::string errLog;
+public:
+	BlockAllPinnedException() { errLog = "All block has been pinned!"; }
+	virtual const char* what()const { return errLog.c_str(); }
+};
+
+//数据块类
+class Block
+{
+private:
+	BYTE data[BLOCKSIZE];
+	bool isDirty;
+	bool isPinned;
+	ADDRESS tag;
+	string filename;
+public:
+	Block():isDirty(false),isPinned(false) {}
+	~Block() {}
+	void setBlockData(BYTE* data) { memcpy(this->data, data, BLOCKSIZE); }
+	BYTE* getBlockData() { return data; }
+	void setDirtyBit(bool isDirty) { this->isDirty = isDirty; }
+	bool isDirtyBit() { return isDirty; }
+	void setPinnedBit(bool pinnnedBit) { this->isPinned = pinnnedBit; }
+	bool isPinnnedBit() { return isPinned; }
+	void setFileName(const std::string& fileName) { this->filename = fileName; }
+	std::string getFileName() { return filename; }
+	void setTag(ADDRESS tag) { this->tag = tag; }
+	ADDRESS getTag() { return tag; }
+	void clear() { isDirty = false; filename = ""; }
+};
 
 class BufferManager
 {
+private:
+	FILE * cur_file;
+	string cur_filename;
+	Block *blocks;
+	ArrayList substitutionQue;
+	void writeABlock(const int& blockIndex);
+	int fetchABlock(const std::string& filename, const ADDRESS& address);
+	int substitute(const std::string& filename, const ADDRESS& tag, BYTE* buffer);
+	int hit(const std::string& fileName, const ADDRESS& tag);
+public:
+	BufferManager();
+	~BufferManager();
+	bool newFile(const std::string& filename);
+	void delFile(const std::string& name);
+	BYTE* fetchARecord(const std::string& name, const ADDRESS& address);
+	void writeARecord(BYTE* record, int recordLength, const std::string& name, const ADDRESS& address);
+	void setBlockPinned(int blockIndex);
+	void setBlockNotPinned(int blockIndex);
+	static BufferManager* getBufferManager();
 };
 
 #endif
