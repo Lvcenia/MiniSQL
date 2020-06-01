2a72883546383711f31bd36235621a73c4ed6860
陈文宇 <cwy1119@qq.com>
2020-06-01 16:38:37 +0800

    实现了ArrayList类(只供BufferManager使用); 实现了部分BufferManager函数

---
+++ MiniSQL/ArrayList.h
@@ -0,0 +1,24 @@
+#ifndef _ARRAYLIST_
+#define _ARRAYLIST_
+
+#define BLOCKSIZE 4096
+#define BLOCKNUM 10
+
+class ArrayList
+{
+private:
+	int head, tail;
+	struct {
+		int last;
+		int next;
+	}arraylist[BLOCKNUM];
+public:
+	ArrayList();
+	~ArrayList();
+	void moveToTail(int index);
+	int getHead();
+};
+#endif // !_ARRAYLIST_
+
+
+
