4d9b102ace552d6bc64a1f948f694db59afaaf73
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2020-06-01 20:20:41 +0800

    定义了RecordManager接口
    建议先按这个做一个简单的出来，之后有时间慢慢加
    ，Condition类是删除和查的条件，没有条件的话传空vector就行

--- MiniSQL/RecordManager.h
+++ MiniSQL/RecordManager.h
@@ -1,5 +1,45 @@
 #pragma once
-class RecordManager
-{
+#include "Generals.h"
+#include "BufferManager.h"
+#include <list>
+#include <vector>
+#include <map>
+
+enum OP {
+	Equal,
+	NotEqual,
+	GreaterThan,
+	LessThan,
+	GreaterEqual,
+	LessEqual
 };
 
+//目前只考虑简单的 （属性 op 值），有时间再加
+class Condition {
+public:
+	Condition(string attributeName, OP op, string value);
+	virtual ~Condition();
+	string getAttributeName();
+	OP getOP();
+	string getValue();
+private:
+	string attributeName;
+	OP op;
+	string value;
+};
+
+class RecordManager
+{
+public:
+	RecordManager();
+	virtual ~RecordManager();
+	const QueryResult& createTable(string tableName, const vector<Attribute>& attributes);
+	const QueryResult& dropTable(string tableName);
+	const QueryResult& insertValues(string tableName, const vector<string>& values);//先只考虑全值插入，也就是values的个数等于属性个数
+	const QueryResult& deleteValues(string tableName, const vector<Condition>& conditions);
+	const QueryResult& selectValues(string tableName, const vector<Condition>& conditions);
+
+private:
+	BufferManager* bufferManager;
+	map<string, Table*> tableDictionary;
+};
