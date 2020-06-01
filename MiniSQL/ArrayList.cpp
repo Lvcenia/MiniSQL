f30452ff661e108dcf28bfa1624c2cdb15746024
陈文宇 <cwy1119@qq.com>
2020-06-01 17:23:58 +0800

    加回了ArrayList类

---
+++ MiniSQL/ArrayList.cpp
@@ -0,0 +1,38 @@
+#include "ArrayList.h"
+
+ArrayList::ArrayList() :
+	head(0), tail(BLOCKNUM - 1)
+{
+	for (int i = 0; i < BLOCKNUM; i++) {
+		arraylist[i].last = i - 1;
+		arraylist[i].next = i + 1;
+	}
+	arraylist[tail].next = -1;
+}
+
+
+ArrayList::~ArrayList()
+{
+}
+
+int ArrayList::getHead()
+{
+	return head;
+}
+
+void ArrayList::moveToTail(int index)
+{
+	if (index == tail)
+		return;
+	int last = arraylist[index].last;
+	int next = arraylist[index].next;
+
+	arraylist[tail].next = index;
+	arraylist[last].next = next;
+	arraylist[next].last = last;
+	arraylist[index].next = -1;
+	arraylist[index].last = tail;
+	tail = index;
+	if (index == head)
+		head = next;
+}
