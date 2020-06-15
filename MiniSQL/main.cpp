f24e874b39eb5d84ef45cda8e34bc23e14b64929
dgz0515 <dgz_3180103554@163.com>
2020-06-15 18:42:28 +0800

    update main

--- MiniSQL/main.cpp
+++ MiniSQL/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 //主函数
 int main()
 {
-	RecordBuffer recordBuffer();
+	RecordBuffer recordBuffer(); // 把它放这里似乎有些不妥，不如放在QueryResult里
 
 	//创建几个Manager对象并各自初始化
 	CatalogManager catalogManager;
@@ -56,6 +56,9 @@ int main()
 				//sql = sql.substr(0, sql.length() - 1);
 				//QueryResult res =  interperter.executeSql(sql);
 				//cout << res.content;
+
+				// 判断res是否是正确的返回，并且是否有返回查询记录
+				// 是，接下来输出查询结果 
 			}
 			cout << "MiniSql->>";
 			sql.clear();
